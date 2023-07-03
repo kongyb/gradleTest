90006bf3a2ededa79274c9db1295bd147ac35c73
KOSA <KOSA@DESKTOP-OR5F96A>
2023-07-03 10:11:50 +0900

    connectionTest추가

---
+++ src/main/java/org/servlet/ConnectionTest.java
@@ -0,0 +1,36 @@
+package org.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dao.TestDAO;
+
+/**
+ * Servlet implementation class ConnectionTest
+ */
+@WebServlet("/connectionTest")
+public class ConnectionTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TestDAO dao = new TestDAO();
+		response.getWriter().write(dao.test());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
